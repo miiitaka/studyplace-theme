fce1a70451a412bfae7214ebb4a23e72e721251d
田崎貴大 <qxgxyea_h@yahoo.co.jp>
2018-07-08 22:31:34 +0900

    ハンバーガーメニューの高さ調整

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,4 +1,6 @@
 $(function() {
+  var windowHeight = $(window).height();
+  $(".global_nav-hamburger").css("height", windowHeight + 'px');
   $(".global_nav-sp img").on("click", function() {
      $(".global_nav-sp").toggleClass("open");
      $(".global_nav-hamburger").toggleClass("open");
