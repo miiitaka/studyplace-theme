68d33d79e6eec93a5be38eae0aa28739d29e370c
Kazuya Takami <foundationmeister@outlook.com>
2018-09-01 15:22:17 +0900

    Google Tag Manager set.

--- header.php
+++ header.php
@@ -4,9 +4,15 @@
 	<meta charset="<?php bloginfo( 'charset' ) ?>">
 	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=0">
 	<meta name="theme-color" content="#004c8c">
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-N59XL6J');</script>
+	<!-- End Google Tag Manager -->
 	<?php wp_head(); ?>
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N59XL6J" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 	<div class="header__wrap">
 		<header class="header">
 			<h1>
