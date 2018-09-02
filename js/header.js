a507bc69c5e951ac28361c8d5c4d670fe424ab13
Kazuya Takami <foundationmeister@outlook.com>
2018-09-02 16:33:14 +0900

    header js file adjust.

--- js/header.js
+++ js/header.js
@@ -1,26 +1,13 @@
-;(function($){
+;(function($) {
 	$(function() {
-		var
-			pos = $('.l-contents__notice').offset(),
-			header = $('.header__wrap2'),
-			start_pos = 0,
-			now_pos = $(this).scrollTop;
+		var elmHeader = $(".header__wrap2");
 
-		headerToggle(header, pos.top, now_pos);
-
-		$(window).on('scroll', function(){
-			now_pos = $(this).scrollTop;
-			headerToggle(header, pos.top, now_pos);
-		});
-
-		function headerToggle(target, target_pos, current_pos) {
-			var now_pos = $(document).scrollTop;
-			if( target_pos < now_pos && current_pos < start_pos ) {
-				target.not(':animated').fadeIn();
+		$(window).on("scroll", function() {
+			if ($(this).scrollTop() > 350) {
+				elmHeader.not(":animated").fadeIn();
 			} else {
-				target.not(':animated').fadeOut();
+				elmHeader.not(":animated").fadeOut();
 			}
-			start_pos = current_pos;
-		}
+		});
 	});
 })(jQuery);
