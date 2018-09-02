1fabd79cf108bb6c89d745370ce89ba67e1d03cf
Kazuya Takami <foundationmeister@outlook.com>
2018-09-02 16:22:42 +0900

    bxslider js  file adjust.

--- js/bxslider.js
+++ js/bxslider.js
@@ -5,19 +5,20 @@
 		if (ua.indexOf("iPhone") > 0 || ua.indexOf("Android") > 0 && ua.indexOf("Mobile") > 0) {
 			var windowSize = $(window).width();
 			$("#media_slider").bxSlider({
-				useCSS: false,
-				minSlides: 2,
+				nextText: "",
 				maxSlides: 2,
+				minSlides: 2,
+				prevText: "",
 				slideWidth: windowSize / 2 - 20
 			});
 		} else {
 			$("#media_slider").bxSlider({
-				useCSS: false,
-				minSlides: 5,
+				nextText: "",
 				maxSlides: 5,
+				minSlides: 5,
+				prevText: "",
 				slideWidth: 200
 			});
-			$(".contents__media .bx-prev, .contents__media .bx-next").text("");
 		}
 	});
 })(jQuery);
