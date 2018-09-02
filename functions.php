23856de89046be6f50c061da11b89e41586c9858
Kazuya Takami <foundationmeister@outlook.com>
2018-09-02 14:45:54 +0900

    bxslider css & js replacement.

--- functions.php
+++ functions.php
@@ -105,15 +105,15 @@ function theme_scripts() {
 	wp_enqueue_style( 'theme-reset',    get_template_directory_uri() . '/css/reset.css', array(), $version );
 	wp_enqueue_style( 'fontawesome',    get_template_directory_uri() . '/css/fontawesome/css/font-awesome.min.css', array(), $version );
 	wp_enqueue_style( 'theme-font',     'https://fonts.googleapis.com/css?family=Vollkorn', array(), $version );
-	wp_enqueue_style( 'bxslider-style', 'https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css', array( 'theme-reset' ), $version );
+	wp_enqueue_style( 'bxslider-style', get_template_directory_uri() . '/css/bxslider/jquery.bxslider.css', array( 'theme-reset' ), $version );
 	wp_enqueue_style( 'theme-style',    get_stylesheet_uri() );
 	wp_enqueue_style( 'theme-responsive-style', get_template_directory_uri() . '/css/responsive.css', array( 'theme-reset', 'theme-style' ), $version );
 
 	wp_deregister_script( 'jquery' );
 	wp_enqueue_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js', array(), $version, false );
-	wp_enqueue_script( 'bxslider',     get_template_directory_uri() . '/js/bxslider.js',  array( 'jquery' ), $version );
-	wp_enqueue_script( 'pagetop-js',   get_template_directory_uri() . '/js/pagetop.js',   array( 'jquery' ), $version );
-	wp_enqueue_script( 'header-js',    get_template_directory_uri() . '/js/header.js',    array( 'jquery' ), $version );
-	wp_enqueue_script( 'hamburger-js', get_template_directory_uri() . '/js/hamburger.js', array( 'jquery' ), $version );
+	wp_enqueue_script( 'bxslider',     get_template_directory_uri() . '/js/jquery.bxslider.js',  array( 'jquery' ), $version );
+	wp_enqueue_script( 'pagetop-js',   get_template_directory_uri() . '/js/pagetop.js',          array( 'jquery' ), $version );
+	wp_enqueue_script( 'header-js',    get_template_directory_uri() . '/js/header.js',           array( 'jquery' ), $version );
+	wp_enqueue_script( 'hamburger-js', get_template_directory_uri() . '/js/hamburger.js',        array( 'jquery' ), $version );
 }
 add_action( 'wp_enqueue_scripts', 'theme_scripts' );
