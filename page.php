89b68ed045a5209386d8534e10aee770d9145715
田崎貴大 <qxgxyea_h@yahoo.co.jp>
2018-09-10 19:27:55 +0900

    ヘッダー色の変更

--- page.php
+++ page.php
@@ -9,6 +9,7 @@
 		$(function(){
 			var pageUrl = location.href;
 			if( pageUrl.indexOf("privacy-policy") !== -1 || pageUrl.indexOf("contact") !== -1) {
+				$(".header__wrap .global_nav").addClass("black");
 				$("#header_logo").attr("src", "<?php echo get_template_directory_uri(); ?>/img/logo2.png");
 			}
 		});
